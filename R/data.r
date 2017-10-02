77437c6a613271e979af991f03ea44bfbf543e03
Jay Hesselberth <jay.hesselberth@gmail.com>
2017-10-02 15:40:08 -0600

    update qpcr

---
+++ R/data.r
@@ -0,0 +1,17 @@
+#' Example Quantitative PCR data
+#'
+#' A dataset containing Bio-Rad qPCR data for IFNbeta and Actin upon
+#' transfection with RNA ligands.
+#'
+#' @format A tibble containing 145 observations of 6 variables.
+#' \describe{
+#'   \item{well}{plate well}
+#'   \item{fluor}{Fluorophore}
+#'   \item{target}{mRNA target}
+#'   \item{content}{Not sure}
+#'   \item{sample}{sample name}
+#'   \item{cq}{Quantification cycle}
+#' }
+#'
+#' @source Rachel Ancar
+"qpcr"
--- data-raw/qpcr.r
+++ data-raw/qpcr.r
@@ -1,8 +1,10 @@
 library(tidyverse)
+library(stringr)
 library(readxl)
 
 qpcr <- read_excel('data-raw/qpcr.xlsx')
-devtools::use_data(qpcr)
+names(qpcr) <- names(qpcr) %>% str_to_lower()
+devtools::use_data(qpcr, overwrite = TRUE)
 
 # Some test analyses
-qpcr %>% spread(Sample, Cq)
+qpcr %>% spread(sample, cq)
