5d4917d3d2b2ce070ce3b6185bc5c98a3981efce
Jay Hesselberth <jay.hesselberth@gmail.com>
2017-10-05 13:48:30 -0600

    more qpcr

--- data-raw/qpcr.r
+++ data-raw/qpcr.r
@@ -1,10 +1,11 @@
 library(tidyverse)
 library(stringr)
 library(readxl)
+library(magrittr)
 
 qpcr <- read_excel('data-raw/qpcr.xlsx')
-names(qpcr) <- names(qpcr) %>% str_to_lower()
-devtools::use_data(qpcr, overwrite = TRUE)
 
-# Some test analyses
-qpcr %>% spread(sample, cq)
+names(qpcr) %<>% str_to_lower()
+
+qpcr %<>% mutate(cq = as.numeric(cq))
+devtools::use_data(qpcr, overwrite = TRUE)
