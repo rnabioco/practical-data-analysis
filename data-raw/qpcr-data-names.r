a7211b5e6626ae5b4e56df84efbf8887a957b152
Jay Hesselberth <jay.hesselberth@gmail.com>
2017-11-29 03:30:46 -0700

    data again

--- data-raw/qpcr-data-names.r
+++ data-raw/qpcr-data-names.r
@@ -7,17 +7,14 @@ rt <- c('+', '-') # reverse transcriptase added?
 rep <- 1:3
 
 set.seed(47681)
-sample_data <- crossing(sample, time, gene, rep, rt) %>%
-  rowwise() %>%
-  mutate(exp = ifelse(sample == "wt", sample(15:20, 1), sample(22:26, 1)))
+sample_data <- crossing(sample, time, gene, rep, rt)
 
 plus_rts <- filter(sample_data, rt == "+")
-minus_rts <- filter(sample_data, rt == "-") %>% mutate(exp = 0)
 
-t0 <- filter(sample_data, time == 0) %>% rowwise() %>% mutate(exp = sample(5:15, 1))
-t12 <- filter(sample_data, time == 12) %>% rowwise() %>% mutate(exp = sample(45:55, 1))
-t24 <- filter(sample_data, time == 24) %>% rowwise() %>% mutate(exp = sample(95:100, 1))
-t48 <- filter(sample_data, time == 48) %>% rowwise() %>% mutate(exp = sample(350:600, 1))
+t0 <- filter(plus_rts, time == 0) %>% rowwise() %>% mutate(exp = sample(5:15, 1))
+t12 <- filter(plus_rts, time == 12) %>% rowwise() %>% mutate(exp = sample(45:55, 1))
+t24 <- filter(plus_rts, time == 24) %>% rowwise() %>% mutate(exp = sample(95:100, 1))
+t48 <- filter(plus_rts, time == 48) %>% rowwise() %>% mutate(exp = sample(350:600, 1))
 
 plus_rts <- bind_rows(t0, t12, t24, t48)
 
@@ -25,6 +22,8 @@ plus_rts <- bind_rows(t0, t12, t24, t48)
 ifns <- filter(plus_rts, gene == "IFN") %>% mutate(exp = exp * 1.5)
 actins <- filter(plus_rts, gene == "ACTIN") %>% mutate(exp = exp * 0.2)
 plus_rts <- bind_rows(ifns, actins)
+
+minus_rts <- filter(sample_data, rt == "-") %>% mutate(exp = 0)
 
 sample_data <- bind_rows(plus_rts, minus_rts) %>% arrange(sample, time, gene, rep)
 
