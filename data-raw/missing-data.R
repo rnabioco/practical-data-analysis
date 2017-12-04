cce32b725f18147c6b004143a48e1491b735d8d7
Jay Hesselberth <jay.hesselberth@gmail.com>
2017-12-03 19:16:42 -0700

    c3 updates

--- data-raw/missing-data.R
+++ data-raw/missing-data.R
@@ -4,8 +4,8 @@ x <- tibble(
   group = c(1:4, 0, NA),
   id = c(1:3, NA, 2, 0),
   name = c("a", "b", NA, "b", "c", "d"),
-  value1 = c(1:5, NA),
-  value2 = c(NA, 4:8)
+  value1 = c(1:4, NA, 10),
+  value2 = c(NA, 5:8, NA)
 )
 
 missing <- x %>% tidyr::complete(group, nesting(id, name)) %>% as_tibble()
